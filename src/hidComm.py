12218629b455dc04acc4f704871e4a9e8ce36479
lolodel <loicdelattre38@gmail.com>
2024-11-29 16:58:43 +0100

    receive data works

--- src/hidComm.py
+++ src/hidComm.py
@@ -6,9 +6,9 @@ import time
 vendor_id = 1046
 product_id = 65535
 
-print("Opening device")
-	
 h = hid.Device(vendor_id, product_id)
+
+print("Opening device")
  
 try:
 	
@@ -25,38 +25,20 @@ try:
     print(byte_str)
     h.write(byte_str)
 		
-    time.sleep(0.05)
+    time.sleep(0.5)
 	
 	# read back some data
-    print("Reading data")   
-    while True:
-        d = h.read(64)
-        if d:
-            print(d)
-        else:
-            break
-			
-    time.sleep(5)
-    
-	# write some data
-    print("Writing data")
-    byte_str = bytes([29, 0xff, 0x55, 0x0b, 0x12, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x02, 0x00, 0x10] + [0x00]*14)
-    print(byte_str)
-    h.write(byte_str)
-	
-    time.sleep(0.05)
-	
-	# read back some data
-    print("Reading data")
-    while True:
-        d = h.read(64)  
-        if d:
-            print(d)
-        else:
-            break
+    #print("Reading data")   
+    #while True:
+        #d = h.read(64)
+        #if d:
+            #print(d)
+        #else:
+            #break
 	
     print("Closing the device")
     h.close()
+    time.sleep(0.5)
 	
 except IOError as ex:
     print(ex)
