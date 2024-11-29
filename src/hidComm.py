db361b1e7886c4b338344b89ba797c9355eb1905
Loic Delattre <loic.delattre@ecam.fr>
2024-11-29 15:41:37 +0100

    writeData func

--- src/hidComm.py
+++ src/hidComm.py
@@ -14,6 +14,14 @@ print("Opening device")
 h = hid.device()
 h.open(vendor_id, product_id)
  
+def writeData(byte_str):
+    print("Writing data")
+    print(byte_str)
+    h.write(byte_str)
+	
+    time.sleep(0.05)
+    pass
+
 try:
 	
     print("Manufacturer: %s" %h.get_manufacturer_string())
@@ -24,12 +32,7 @@ try:
     h.set_nonblocking(1)
 	
 	# write some data
-    print("Writing data")
-    byte_str = bytes([29, 0xff, 0x55, 0x0b, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x02, 0x00, 0x10] + [0x00]*14)
-    print(byte_str)
-    h.write(byte_str)
-		
-    time.sleep(0.05)
+    writeData(bytes([29, 0xff, 0x55, 0x0b, 0x12, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x02, 0x00, 0x10] + [0x00]*14))
 	
 	# read back some data
     print("Reading data")   
@@ -43,12 +46,7 @@ try:
     time.sleep(5)
     
 	# write some data
-    print("Writing data")
-    byte_str = bytes([29, 0xff, 0x55, 0x0b, 0x12, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x02, 0x00, 0x10] + [0x00]*14)
-    print(byte_str)
-    h.write(byte_str)
-	
-    time.sleep(0.05)
+    writeData(bytes([29, 0xff, 0x55, 0x0b, 0x12, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x02, 0x00, 0x10] + [0x00]*14))
 	
 	# read back some data
     print("Reading data")
@@ -59,6 +57,7 @@ try:
         else:
             break
 	
+    
     print("Closing the device")
     h.close()
 	
